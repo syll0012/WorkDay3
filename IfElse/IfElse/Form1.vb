3c3305c891cabfe521ac12f67bbbabc1266ae875
syll0012 <syll0012@umn.edu>
2013-09-23 15:26:38 -0500

    work day 3 september 23

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -5,18 +5,31 @@ Public Class Form1
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
         Dim num1, num2, largerNum As Double
 
-        num1 = CDbl(txtFirstNum.Text)
-        num2 = CDbl(txtSecondNum.Text)
-        'Take the input and immediately convert it into a double'
-        'For comparing numbers'
-        If (num1 > num2) Then
-            largerNum = num1
-            txtResult.Text = "The larger number is " & num1
-        ElseIf (num1 < num2) Then
-            txtResult.Text = "The larger number is" & num2
+        'Now, we want to make sure it doesn't crash if one were to accidently put in letters that can't be doubled. Both
+        'of the IsNumeric statements have to be true in order for it to proceed'
+        If IsNumeric(txtFirstNum.Text) And IsNumeric(txtSecondNum.Text) Then
+            num1 = CDbl(txtFirstNum.Text)
+            num2 = CDbl(txtSecondNum.Text)
+
+            'Take the input and immediately convert it into a double'
+            'For comparing numbers'
+            'Return will take you to the end of the program if it fails' 'it will take you to End Sub'
+            'Return'
+
+
+            If (num1 > num2) Then
+                largerNum = num1
+                txtResult.Text = "The larger number is " & num1
+            ElseIf (num1 < num2) Then
+                txtResult.Text = "The larger number is " & num2
+            Else
+                txtResult.Text = "They are equal"
+            End If
         Else
-            txtResult.Text = "They are equal"
+            MessageBox.Show("Error: please type a number")
         End If
+
+
 
     End Sub
 End Class
